4cfd57b139c84ff8ca9e6a0e74d3e49cd937e69b
evolv34 <chan@evolv.com>
2017-05-14 00:10:45 -0500

    remove unused files

--- docker-pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true; do
-		
-		docker pull chanhub/kafka-realtime
-		docker rmi -f $(docker images -a | grep "chanhub/kafka-realtime" | awk '{print $3}')
-	done
--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-#docker rm -f $(docker ps -aq)
+./gradlew clean build 
+
+docker build -t evolv/kakfa-websocket .
+
+docker rm -f $(docker ps -aq)
 
 docker-compose -f docker-compose-kafka.yml up
